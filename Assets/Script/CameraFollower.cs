816a6dca9b47ed7e5c62591b2ae2f68e0f48f4f9
Tarjitay <96352246+Tarjitay@users.noreply.github.com>
2021-12-19 08:06:22 +0300

    asd

---
+++ Assets/Script/CameraFollower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform Snake;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - Snake.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 transformPosition = new Vector3(0, Snake.position.y + offset.y, Snake.position.z + offset.z);
+        transform.position = transformPosition;
+    }
+}
