6781c1c3e9318726f087c6b2c06c83a00bc18b20
Tarjitay <96352246+Tarjitay@users.noreply.github.com>
2021-12-19 21:27:32 +0300

    Snake Length and eat

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float move = 15f;
     public Rigidbody rd;
-    
+
+    public int Length = 1;
+    public TextMeshPro Number;
+    public GameObject Food;
+
+    private SnakeTail componentSnakeTail;
+
     void Start()
     {
         rd = GetComponent<Rigidbody>();
+        componentSnakeTail = GetComponent<SnakeTail>();
+
+        for (int i = 1; i < Length; i++) componentSnakeTail.AddSphere();
+
+        Number.SetText(Length.ToString());
     }
 
     void Update()
@@ -17,5 +29,24 @@ public class PlayerController : MonoBehaviour
         float directional = Input.GetAxis("Horizontal");
         Vector3 motion = new Vector3(move * directional, 5f);
         rd.velocity = motion;
+
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+            //Length--;
+            //componentSnakeTail.RemoveSphere();
+            //Number.SetText(Length.ToString());
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out Food food))
+        {
+            Destroy(other.gameObject);
+
+            Length++;
+            componentSnakeTail.AddSphere();
+            Number.SetText(Length.ToString());
+        }
     }
 }
---
+++ Assets/Script/SnakeTail.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTail : MonoBehaviour
+{
+    public Transform SnakeHead;
+    public float SphereDiameter;
+
+    private List<Transform> snakeSpheres = new List<Transform>();
+    private List<Vector3> positions = new List<Vector3>();
+
+    private void Awake()
+    {
+        positions.Add(SnakeHead.position);
+    }
+
+    private void Update()
+    {
+        float distance = ((Vector3) SnakeHead.position - positions[0]).magnitude;
+
+        if (distance > SphereDiameter)
+        {
+            Vector3 direction = ((Vector3) SnakeHead.position - positions[0]).normalized;
+
+            positions.Insert(0, positions[0] + direction * SphereDiameter);
+            positions.RemoveAt(positions.Count - 1);
+
+            distance -= SphereDiameter;
+        }
+
+        for (int i = 0; i < snakeSpheres.Count; i++)
+        {
+            snakeSpheres[i].position = Vector3.Lerp(positions[i + 1], positions[i], distance / SphereDiameter);
+        }
+    }
+
+    public void AddSphere()
+    {
+        Transform sphere = Instantiate(SnakeHead, positions[positions.Count - 1], Quaternion.identity, transform);
+        snakeSpheres.Add(sphere);
+        positions.Add(sphere.position);
+    }
+
+    public void RemoveSphere()
+    {
+        Destroy(snakeSpheres[0].gameObject);
+        snakeSpheres.RemoveAt(0);
+        positions.RemoveAt(1);
+    }
+}
